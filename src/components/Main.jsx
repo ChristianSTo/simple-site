084e428b9f0676be970a86e74de863492dd368c5
ChristianTo <christian.to@outlook.com>
2025-12-07 21:42:00 -0600

    initial

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../blocks/main.css";
+import NavBar from "../components/NavBar";
+function Main() {
+  return (
+    <section className="main space">
+      <NavBar />
+    </section>
+  );
+}
+
+export default Main;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "../blocks/navBar.css";
+
+function NavBar() {
+  return (
+    <>
+      <section className="nav__text">Hello</section>
+    </>
+  );
+}
+
+export default NavBar;
